cf2da876fc75df652673e2f765465aefac82f81e
unknown <gabrieldonner@gmail.com>
2025-01-04 00:05:43 -0300

    home-page 50% concluida

---
+++ script.js
@@ -0,0 +1,16 @@
+ /* const TotalGrupos = 10;
+let baseGrupos = 0;
+
+const gruposContador = document.querySelector("#contagemGrupos");
+
+function updateContagemGrupos() {
+
+    if(baseGrupos <= TotalGrupos) {
+        baseGrupos++;
+        gruposContador.textContent = baseGrupos;
+    } else {
+        clearInterval(gruposInterval);
+    }
+
+}
+const gruposInterval = setInterval(updateContagemGrupos, 200); */
