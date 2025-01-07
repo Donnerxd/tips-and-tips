cc6958e055ea4af1191273acc79bd08a948fc22b
unknown <gabrieldonner@gmail.com>
2025-01-07 01:11:18 -0300

    start aba ranking

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function updateContagemGrupos() {
 
 }
 const gruposInterval = setInterval(updateContagemGrupos, 200); */
+
+// Adicionar eventos de clique buttons dropdown
+document.querySelectorAll('.dropdown-button').forEach(button => {
+    button.addEventListener('click', function (event) {
+      // Fecha todos os menus abertos antes de abrir o atual
+      document.querySelectorAll('.dropdown-options').forEach(menu => {
+        // Esconde o menu que não foi clicado
+        if (menu !== this.nextElementSibling) {
+          menu.style.display = 'none';
+        }
+      });
+
+      // Alterna o menu associado ao botão clicado
+      const dropdownMenu = this.nextElementSibling;
+      dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+
+      // Impede que o clique no botão feche o menu imediatamente
+      event.stopPropagation();
+    });
+  });
+
+  // Fechar todos os menus ao clicar fora
+  window.addEventListener('click', function () {
+    document.querySelectorAll('.dropdown-options').forEach(menu => {
+      menu.style.display = 'none';
+    });
+  });
