6581b559a130538779f43e3c5f298fa674057895
unknown <gabrieldonner@gmail.com>
2025-01-07 16:31:34 -0300

    aaa

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
- /* const TotalGrupos = 10;
-let baseGrupos = 0;
-
-const gruposContador = document.querySelector("#contagemGrupos");
-
-function updateContagemGrupos() {
-
-    if(baseGrupos <= TotalGrupos) {
-        baseGrupos++;
-        gruposContador.textContent = baseGrupos;
-    } else {
-        clearInterval(gruposInterval);
-    }
-
-}
-const gruposInterval = setInterval(updateContagemGrupos, 200); */
-
 // Adicionar eventos de clique buttons dropdown
 document.querySelectorAll('.dropdown-button').forEach(button => {
     button.addEventListener('click', function (event) {
